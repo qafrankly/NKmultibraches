b3eb812514da7fe62c1f1ae7d3b40dc1fac08111
qafrankly <31970630+qafrankly@users.noreply.github.com>
2018-01-30 17:01:23 +0200

    Update index.js

--- twix/index.js
+++ twix/index.js
@@ -1,16 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 
-class MultiDemo12 extends Component {
+class nadkh2 extends Component {
 	
   render(){
 	  
     return (
-      <div className='FranklySecondModulesDemo'>
-	      <h2 className='demo_text'>nad2: {this.props.text}</h2>
+      <div className='nadiakhouritwo'>
+	      <h2 className='demo_text'>twix2: {this.props.text}</h2>
       </div>
     );
 	  
   }
 }
 
-export default MultiDemo12;
+export default nadkh2;
